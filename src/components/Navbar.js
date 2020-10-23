ac80c7deb5a94786cb95a86e99103eecc0ef1946
Jc7j <chiangjason19@gmail.com>
2020-10-22 22:28:28 -0700

    Fixed navbar logo to stay in navbar when screen size is smaller

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import LogoSmall from '../images/logo-small.png';
+
 import Button from './Button';
 
 import './Navbar.css';
@@ -31,10 +33,7 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            <img
-              src={require('../images/logo-small.png')}
-              alt="LV Town Fridge"
-            ></img>
+            <img src={LogoSmall} alt="LV Town Fridge"></img>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
