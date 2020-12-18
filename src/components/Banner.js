5276a3160d4a403be445ad41c6f40c7eec11a0d4
Jc7j <chiangjason19@gmail.com>
2020-12-17 20:25:19 -0800

    Added prettier and .vscode/settings.json. Added a styles folder and changed HomePage a little.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,30 +1,23 @@
 import React from "react";
-import "../App.css";
+
 import { Button } from "./Button";
-import { Link } from "react-router-dom";
-import "./Banner.css";
-import "./Button.css";
+
+import "../styles/Banner.css";
 
 export default function Banner() {
   return (
-    <div>
-      <div className="container">
-        <div className="row">
+      <div className="bannerContainer">
           <div className="four columns">
             <div className="mini-cta ">
               <h5>Take what you need + leave what you can.</h5>
 
-              {Button && (
                 <Button buttonStyle="btn--outline" to="/donate">
                   Get Involved
                 </Button>
-              )}
 
-              {Button && (
                 <Button buttonStyle="btn--outline" to="/donate">
                   Donate
-                </Button>
-              )}
+                 </Button>
             </div>
           </div>
 
@@ -36,7 +29,5 @@ export default function Banner() {
             ></img>
           </div>
         </div>
-      </div>
-    </div>
   );
 }
