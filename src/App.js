d447a13d405a5a8e7c02118096edff5e3325714e
Jc7j <chiangjason19@gmail.com>
2021-01-02 21:19:11 -0800

    Removed Fridge page file. Added About page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
+
 import Footer from './components/Footer';
-import Fridges from './pages/Fridges';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
 
 import './App.css';
+
 function App() {
   return (
     <div className="App">
@@ -13,7 +15,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/fridges" exact component={Fridges} />
+          <Route path="/about" exact component={About} />
         </Switch>
         <Footer />
       </Router>
