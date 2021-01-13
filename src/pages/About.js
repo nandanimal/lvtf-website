86f3025ee7caa20fc799a38cf0c8f12e989bf921
Jc7j <chiangjason19@gmail.com>
2021-01-13 13:44:40 -0800

    Added news section to the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,52 +1,102 @@
 import React from 'react';
 
-import "../styles/About.css";
+import {Button} from '../components/Button';
 
-const textStyles = {fontSize: '20px', marginBottom: '.8em'}
+import '../styles/About.css';
+
+const textStyles = {fontSize: '20px', marginBottom: '.8em'};
+const newsList = [
+  {
+    image: require('../images/LVRJ-logo.png'),
+    alt: 'LVRJ logo',
+    date: 'October 26, 2020',
+    title: 'Las Vegas Town Fridge provides 24-hour access to food',
+    link:
+      'https://www.reviewjournal.com/entertainment/food/las-vegas-town-fridge-provides-24-hour-access-to-food-2162640/#:~:text=The%20Las%20Vegas%20Town%20Fridge,those%20who%20wish%20to%20donate.&text=The%20idea%20behind%20the%20Las,any%20time%2C%20day%20or%20night.',
+  },
+  {
+    image: require('../images/eater-logo.png'),
+    alt: 'eater logo',
+    date: 'September 23, 2020',
+    title: 'A New Community Fridge Lets Anyone Take What Food They Need 24/7',
+    link: 'https://vegas.eater.com/2020/9/23/21452546/community-fridge-24-7',
+  },
+  {
+    image: require('../images/13-logo.png'),
+    alt: '13 logo',
+    date: 'September 20, 2020',
+    title: 'Las Vegas Town Fridge helps feed those in need',
+    link: 'https://www.ktnv.com/positivelylv/las-vegas-town-fridge-helps-feed-those-in-need',
+  },
+];
 
 const About = () => {
-    return (
-        <div className="aboutContainer">
-            {/* <video>
+  return (
+    <div className="aboutContainer">
+      {/* <video>
                 <source src={require("../images/video.mov")} type="video/mp4"/>
                 Your browser does not support the video tag.
             </video> */}
-            <div className="ourStoryContainer">
-                <p style={{ fontSize: '35px', marginBottom: '.8em'}}>Our story</p>
-                <p style={textStyles}>Take a trip to your local supermarket, restaurant kitchen, 
-                    or maybe even your own refrigerator and pantry, and you'll see what seems to be an abundance of food. 
-                    So much of that food, however, never reaches the people that really need it.
-                </p>
-                <p style={textStyles}>
-                    In other words, the problem is no longer about production--it's about distribution.
-                </p>
-                <p style={textStyles}>
-                    That being said, people can donate food to one another. Here in Nevada, Individual 
-                    and nonprofits are protected under the Bill Emerson Good Samaritan Food Donation
-                    Act, which means that you, me, or anyone else for that matter ...
-                </p>
-                <p style={textStyles}>
-                    So why don't we?
-                </p>
-                <p style={textStyles}>
-                    There's a lot of reasons - too many to hash out here, but again, most stem from the issue of 
-                    distribution. That's where (we think) community fridges come in.
-                </p>
-                <div className="textWithSideImage">
-                    <img src={require('../images/fridge.jpg')} alt='Main Street Fridge Picture' className='ourStoryFridgePic'/>
-                    <div style={{display: 'flex', flexDirection: 'column'}}>
-                        <p style={textStyles}>A community fridge streamlines the process of donating food, opening the
-                        door for anyone to easily help out in their community. Fridges are additional points of access - not just for people
-                        who take from the fridges, but also those who can provide to them. Whereas a traditional food bank can accept
-                        certain foods and supplies via federally subsidized contracts (and has high set up and operations costs), a
-                        community fridge is an easy, flexible, and cheap alternative.</p>
-                        <p style={textStyles}>By working to understand what is happening with otherwise predestined food waste and the role we
-                        play in it, we can begin to see how we can instead work to address food insecurity and fight food waste at the same time.</p>
-                    </div>
-                </div>
-            </div>
+      <div className="ourStoryContainer">
+        <p style={{fontSize: '35px', marginBottom: '.8em'}}>Our story</p>
+        <p style={textStyles}>
+          Take a trip to your local supermarket, restaurant kitchen, or maybe even your own refrigerator and
+          pantry, and you'll see what seems to be an abundance of food. So much of that food, however, never
+          reaches the people that really need it.
+        </p>
+        <p style={textStyles}>
+          In other words, the problem is no longer about production--it's about distribution.
+        </p>
+        <p style={textStyles}>
+          That being said, people can donate food to one another. Here in Nevada, Individual and nonprofits
+          are protected under the Bill Emerson Good Samaritan Food Donation Act, which means that you, me, or
+          anyone else for that matter ...
+        </p>
+        <p style={textStyles}>So why don't we?</p>
+        <p style={textStyles}>
+          There's a lot of reasons - too many to hash out here, but again, most stem from the issue of
+          distribution. That's where (we think) community fridges come in.
+        </p>
+
+        <img
+          src={require('../images/fridge.jpg')}
+          alt="Main Street Fridge Picture"
+          className="ourStoryFridgePic"
+        />
+        <p style={textStyles}>
+          A community fridge streamlines the process of donating food, opening the door for anyone to easily
+          help out in their community. Fridges are additional points of access - not just for people who take
+          from the fridges, but also those who can provide to them. Whereas a traditional food bank can accept
+          certain foods and supplies via federally subsidized contracts (and has high set up and operations
+          costs), a community fridge is an easy, flexible, and cheap alternative.
+        </p>
+        <p style={textStyles}>
+          By working to understand what is happening with otherwise predestined food waste and the role we
+          play in it, we can begin to see how we can instead work to address food insecurity and fight food
+          waste at the same time.
+        </p>
+        <div style={{float: 'right'}}>
+          <Button buttonStyle="btn--outline">Get Involved</Button>
+          <Button buttonStyle="btn--outline">Donate</Button>
         </div>
-    )
-}
+      </div>
+
+      <div className="newsContainer">
+        <p style={{fontSize: '35px', marginBottom: '.8em', paddingLeft: '1em'}}>News</p>
+        {newsList.map((news) => {
+          return (
+            <a href={news.link} target="_blank" className="individualNewsContainer">
+              <img src={news.image} alt={news.alt} className="newsImage" />
+              <div style={{flexDirection: 'column'}}>
+                <p style={{fontSize: '15px', color: 'grey'}}>{news.date}</p>
+                {news.title}
+              </div>
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,57 +9,60 @@ const Home = () => {
   return (
     <div className="homeContainer">
       <div className="bannerContainer">
-        <img className="wavyBackgroundImage" src={require("../images/background-wavy.png")} alt="wavy background"/>
+        <img
+          className="wavyBackgroundImage"
+          src={require('../images/background-wavy.png')}
+          alt="wavy background"
+        />
         <div className="takeWhatYouNeedContainer">
-          <p style={{fontSize: '35px', fontWeight: 800}}>Take what you need, + leave what you can.</p> 
+          <p style={{fontSize: '35px', fontWeight: 800}}>Take what you need, + leave what you can.</p>
           <div style={{display: 'flex', justifyContent: 'flex-end'}}>
             {/* @TODO where link to */}
             <Button buttonStyle="btn--outline" to="">
-                  Get Involved
+              Get Involved
             </Button>
 
             <Button buttonStyle="btn--outline" to="">
-                  Find a Fridge
+              Find a Fridge
             </Button>
           </div>
         </div>
-        <img id="lvtfwoman-img" src={require('../images/lvtf-shirt-cropped.png')} alt="LVTF Woman"/>
+        <img id="lvtfwoman-img" src={require('../images/lvtf-shirt-cropped.png')} alt="LVTF Woman" />
       </div>
 
       <div className="missionContainer">
-        <img id="lvtf-logo" src={require('../images/logo-circle.png')} alt="lvtf-logo"/>
+        <img id="lvtf-logo" src={require('../images/logo-circle.png')} alt="lvtf-logo" />
         <div className="missionText">
           <p style={{fontSize: '35px', fontWeight: 800}}>Our Mission</p>
-          <p style={{fontSize: '25px'}}>The Las Vegas Town Fridge Project is a collective looking to nourish our communities. We are your neighbors with a mission to combat food security and waste.
-
-Our volunteers work within their own communities to collaborate with local, like-minded partners to place community refrigerators across the city.</p>
-            <Button buttonStyle="btn--outline">
-              Read more
-            </Button>
+          <p style={{fontSize: '25px'}}>
+            The Las Vegas Town Fridge Project is a collective looking to nourish our communities. We are your
+            neighbors with a mission to combat food security and waste. Our volunteers work within their own
+            communities to collaborate with local, like-minded partners to place community refrigerators
+            across the city.
+          </p>
+          <Button buttonStyle="btn--outline">Read more</Button>
         </div>
       </div>
 
       <div className="mapContainer">
         <p style={{fontSize: '35px', fontWeight: 800, marginBottom: '.8rem'}}> Find a Fridge</p>
         <div className="mapAndText">
-          <Map/>
+          <Map />
           <div className="fridgeTextLocations">
             <span>
               <p>UMOK</p>
-              <p style={{ fontSize: '25px', color: '#888888'}}>1010 N Main Street</p>
+              <p style={{fontSize: '25px', color: '#888888'}}>1010 N Main Street</p>
             </span>
 
             <span>
               <p>Project4Humanity</p>
-              <p style={{ fontSize: '25px', color: '#888888'}}>912 E Sahara</p>
+              <p style={{fontSize: '25px', color: '#888888'}}>912 E Sahara</p>
             </span>
 
-           <div className="interestedContainer">
-            <p style={{ fontSize: '25px'}}>Interested in hosting a fridge?</p>
-            <Button buttonStyle="btn--outline">
-              Click here
-            </Button>
-           </div>
+            <div className="interestedContainer">
+              <p style={{fontSize: '25px'}}>Interested in hosting a fridge?</p>
+              <Button buttonStyle="btn--outline">Click here</Button>
+            </div>
           </div>
         </div>
       </div>
